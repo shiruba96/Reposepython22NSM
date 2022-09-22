874f77361c0fef415170d733b59cf4764216c139
Nancy Silva <31602174+shiruba96@users.noreply.github.com>
2022-09-22 08:37:03 -0500

    area y volumen de una esfera

---
+++ prog5.py
@@ -0,0 +1,9 @@
+# calcular el volumen y área de una esfera.
+
+print ("Ingrese el valor del radio de la esfera")
+r = float (input ())
+#Calcular area de una esfera
+a = 4 * (3.1416) * (r * r)
+#calcular volumen de una esfera 
+v = (4/3) * (3.1416) * (r * r *r)  
+print ("el volumen es ", v, "el area es", a)
