eb6a959393a1e2bc15ec96b47e7cd2f555d44576
dinaortiz24 <53024899+dinaortiz24@users.noreply.github.com>
2022-09-27 09:29:03 -0500

    Correción de fórmula valor futuro

--- 2prog4.py
+++ 2prog4.py
@@ -1,17 +1,18 @@
-# futuro valor de una cantidad de dinero, a partir del capital iniciall, 
+# futuro valor de una cantidad de dinero, a partir del capital iniciall,
 # tipo de interes y el numero de años
+import math
 
-print ("Ingrese el capital inicial")
-ci = float (input ())
-print ("Ingrese el tipo de interes que se manejar")
-i = float (input ())
-print ("Ingrese la cantidad de años a calcular")
-a = int (input ())
-#Calcular el porcentaje del capital con interes
-p = (ci * i)/100
+print("Ingrese el capital inicial")
+ci = float(input())
+print("Ingrese el tipo de interes que se manejar")
+i = float(input())
+print("Ingrese la cantidad de años a calcular")
+a = int(input())
+# Calcular el porcentaje del capital con interes
+#p = (ci * i)/100
 # calculr el porcentaje segun los años
-a2 = p * a
-#valo futuro
-vf = ci + a2
+#a2 = p * a
+# valo futuro
+vf = ci*pow((1+i/100), a)
 
-print ("el total es ", vf)
+print("el total es ", vf)
