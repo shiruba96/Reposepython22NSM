fc459cb30dfcef2df005288153c8cc57d93b21ab
Nancy Silva <31602174+shiruba96@users.noreply.github.com>
2022-10-04 08:39:27 -0500

    Delete EjerciciosBasicos2 directory

--- EjerciciosBasicos2/2prog1.py
+++
@@ -1,8 +0,0 @@
-#Aceptar el radio de un circulo y compute su area 
-
-print ("Ingrese el valor del radio del circulo")
-r = float (input ())
-#Calcular area 
-r2 = (r * r)
-a = (3.1416) * r2
-print ("el area es", a)
--- EjerciciosBasicos2/2prog3.py
+++
@@ -1,9 +0,0 @@
-#Introducir base y altura de un triangulo y compute su area
-
-print ("Ingrese la base del triangulo")
-b = float (input ())
-print ("Ingrese la altura del triangulo")
-h = float (input ())
-#Calcular area 
-a = (b * h)/2  
-print ("el area es", a)
