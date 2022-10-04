87582befa9df9a95092a008f57badcfab779a368
Nancy Silva <31602174+shiruba96@users.noreply.github.com>
2022-10-04 08:54:05 -0500

    Delete prog3.py

--- prog3.py
+++
@@ -1,7 +0,0 @@
-# convertir de grados Fahrenheit a grados Celsius.
-
-print ("Ingrese la temperatura en grados Fahrenheit")
-f= float (input ())
-#formula para convertir F a C
-c = (f - 32) / 1.8
-print ("son", c, "grados celsius")
