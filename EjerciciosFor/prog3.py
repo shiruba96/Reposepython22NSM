09994f0e2aa7abdafcd5465ea2a578365c070ab6
Shiruba <nancy.silva.2396@gmail.com>
2022-10-13 08:47:12 -0500

    act-for

--- EjerciciosFor/prog3.py
+++ EjerciciosFor/prog3.py
@@ -1,16 +1,16 @@
 #Elabore un programa que permita la entrada de 100 números enteros y determine cual de ellos es un número primo.
 #cambiar el 11 del ramgo por un 101
 
+import random
 print("Ingresa una cadena de 100 numeros: ")
 cont = 1
-primo = 0
-for cont in range(1,11):
-    print ("ingrerese el numero ", cont)
-    num = int (input ())
+primo = ""
+for cont in range(1,101):
+    
+    num = random.randint (1,1000)
+    #print ("ingrerese el numero ", cont)
+    #num = int (input ())
     x = num
-    cont +=1
     if x % 2 != 0:
-        primo = x
-    print (primo, "es un numero primo")
-#    else :
-#        primo = 0
+        primo = primo + str(x) + " "
+print ("los numero primos son", primo)
